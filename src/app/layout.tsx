eec4693b5e51f5cd28949a581e377709d7b34386
Owonikoko Olaoluwa <kokoserver@hotmail.com>
2024-05-20 12:34:48 +0100

    init nivabit setup with tailwind and chadcn components library

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Nivabit",
+  description: "Your home of innovation and solution",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={cn("min-h-screen font-sans", fontSans.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+}
