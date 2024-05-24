897fff905da7d3473bdbef1a3ef4a5439f9d8e86
Owonikoko Olaoluwa <kokoserver@hotmail.com>
2024-05-24 22:50:26 +0100

    add components to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import BannerSection from "@/components/home/BannerSection";
+import Client from "@/components/home/ServicesCarouselSpacing";
 import MeetUs from "@/components/home/MeetUs";
+import Services from "@/components/home/Services";
+import ClientCarouselSpacing from "@/components/home/ClientCarouselSpacing";
+import Portfolio from "@/components/home/Portfolio";
+import Testimonials from "@/components/home/Testimonials";
 
 export default function Home() {
   return (
-    <main className='px-20'>
+    <main>
       <BannerSection />
       <MeetUs />
+      <Client />
+      <Services />
+      <ClientCarouselSpacing />
+      <Portfolio />
+      <Testimonials />
     </main>
   );
 }
