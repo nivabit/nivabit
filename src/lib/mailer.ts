ec11a3833b8ef80da32684383c98b4c77eaebb76
kokoserver <kokoserver@hotmail.com>
2024-11-22 00:18:12 +0100

    update mail server

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,21 +1,23 @@
 import nodemailer from "nodemailer";
 
-const email = process.env.EMAIL;
-const pass = process.env.EMAIL_PASS;
+const email = process.env.SMTP_USERNAME;
+const pass = process.env.SMTP_PASSWORD;
+const host = process.env.SMTP_HOST;
+const port = process.env.SMTP_PORT;
+
 export const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-                user: email,
-                pass,
-        },
+  host: host,
+  port: port,
+  secure: false,
+  auth: {
+    user: email,
+    pass,
+  },
 });
 
 export const mailOptions = {
-        from: email,
-        to: email,
+  from: email,
+  to: email,
 };
 
-export type contactType = { name: string, services?: string[], email: string, subject: string, message?: string, company?: string }
-
+export type contactType = { name: string; services?: string[]; email: string; subject: string; message?: string; company?: string };
