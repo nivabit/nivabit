7d16bb9f84b12741b2b92de05c25a3a3375954b9
kokoserver <kokoserver@hotmail.com>
2026-02-18 03:10:41 +0100

    feat: Add Meta Pixel tracking to the landing page.

--- src/routes/landing/+page.svelte
+++ src/routes/landing/+page.svelte
@@ -77,6 +77,37 @@
 		name="description"
 		content="Get a high-converting website that turns visitors into revenue. Mobile-friendly, fast, and professional web design services for high-growth businesses."
 	/>
+	<!-- Meta Pixel Code -->
+	<noscript
+		><img
+			height="1"
+			width="1"
+			alt="pixel"
+			style="display:none"
+			src="https://www.facebook.com/tr?id=1952710342260113&ev=PageView&noscript=1"
+		/></noscript
+	>
+	<!-- End Meta Pixel Code -->
+	<script>
+		!(function (f, b, e, v, n, t, s) {
+			if (f.fbq) return;
+			n = f.fbq = function () {
+				n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+			};
+			if (!f._fbq) f._fbq = n;
+			n.push = n;
+			n.loaded = !0;
+			n.version = '2.0';
+			n.queue = [];
+			t = b.createElement(e);
+			t.async = !0;
+			t.src = v;
+			s = b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t, s);
+		})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '1952710342260113');
+		fbq('track', 'PageView');
+	</script>
 </svelte:head>
 
 <main class="min-h-screen bg-brand-blue-900 text-white">
